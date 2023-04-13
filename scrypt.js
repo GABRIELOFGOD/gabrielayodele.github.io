4fdcea72ca1bdb78ac6f999d20ca3bb9821bcd5c
GABRIELOFGOD <114562466+GABRIELOFGOD@users.noreply.github.com>
2023-04-13 11:31:00 +0100

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,7 @@
+/*const bars = document.getElementById("navbar-ball")
+const items = document.querySelector(".sections-itm")
+
+
+bars.addEventListener("click", function(){
+    items.style.translateX = "-250px";
+})
